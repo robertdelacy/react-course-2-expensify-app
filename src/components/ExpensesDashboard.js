66dfdd7fa412ad074cd4f9d1993a95419db615fd
De Lacy <P10491445@capita.co.uk>
2019-04-03 12:35:22 +0100

    Setup production webpack build

--- src/components/ExpensesDashboard.js
+++ src/components/ExpensesDashboard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
-//import 'react-dates/lib/css/_datepicker.css';
 
 const ExpenseDashboardPage = (props) => (
     <div>

78dbd31359ffcc80b66bd0b3fbb98fada3a0ed4f
De Lacy <P10491445@capita.co.uk>
2019-04-04 09:48:43 +0100

    Fixed stupid mistake with snapshot testing

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -11,4 +11,13 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     </div>
 );
 
+export const Test = () => (
+    <div>
+        <Link to={`/edit/10`}>
+            <h3>Coffee</h3>
+        </Link>
+        <p>£3.10 12th February, 2019</p>
+    </div>
+);
+
 export default ExpenseListItem;
--- src/tests/components/expenselistitem.test.js
+++ src/tests/components/expenselistitem.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import ExpenseListItem from '../../components/ExpenseList'
+import ExpenseListItem from '../../components/ExpenseListItem';
 import { expenses } from '../fixtures';
 
+let wrapper;
+
+beforeEach(() => {
+    wrapper =  shallow(<ExpenseListItem {...expenses[0]}/>);
+});
+
 test('should render ExpenseListItem with expenses', () => {
-    const wrapper = shallow(<ExpenseListItem {...expenses[1]}/>);
     expect(wrapper).toMatchSnapshot();
 });
